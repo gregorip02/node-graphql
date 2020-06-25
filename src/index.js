8683406e882ac63205f35da49d7701a7d6ebbe58
Gregori Piñeres <gregori.pineres02@gmail.com>
2020-06-25 18:18:19 -0400

    Add docker support

--- src/index.js
+++ src/index.js
@@ -11,7 +11,15 @@ require('dotenv').config({
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs: Types, resolvers: Resolvers });
 
+const port = process.env.PORT || 4000
+
 // The `listen` method launches a web server.
-server.listen({ port: proccess.env.PORT || 4000 }).then(({ url }) => {
+server.listen({ port }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
+
+  if (process.env.EXTERNAL_PORT) {
+    const externalUrl = url.replace(port, process.env.EXTERNAL_PORT);
+
+    console.log(`📤 Exposing at ${externalUrl}`);
+  }
 });
